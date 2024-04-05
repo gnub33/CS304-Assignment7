490dc53c7ed7698446633549460968ab87788f9b
Daniel <gnub33@gmail.com>
2024-04-04 22:22:36 -0400

    new file

--- summ.c
+++ summ.c
@@ -1,31 +1,35 @@
+//Daniel Knight
+//CS304
+//Assignment 7
+
+
 #include <stdio.h>
 
-
+//summation loop
 int summation (int n) 
 { 
-    int sum = -2;
+    int sum = -2; 
 
-    for (int i = 0; i < n; n--) {
+    for (int i = 0; i < n; n--) { //decrement from n to 0
         int product = 0;
         int op = n;
-        while(op > 0 ) {
+        while(op > 0 ) { //addition loop to simulate multiplication
             product += 5;
             op--;
         }
         product -= 2; 
-        sum += product; 
+        sum += product; //running total
     }
     return sum;
 }
 
-
 int main() 
 {
-    int upperValue = 10;
-    int n = upperValue;
+    
+    int n = 10; //hardcoded to 10
 
     for (int i = 0; i <= n; i++) {
-        int sum = summation(i);
+        int sum = summation(i); //call summation loop
         printf("%2d : %5d  0x%08x\n", i, sum, sum);
     }
 
